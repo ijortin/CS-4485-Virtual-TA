a4c4ebe7185b468a40a216b71adc2a1a66d8bcf6
Elizabeth Clements <elizabeth.clementsclan@gmail.com>
2022-10-18 23:22:21 -0700

    First Equation Renderings
    Here is the app with the associated packages to get equations to render. Currently a placeholder constant equation is being rendered until dynamic implementation

--- Virtual-TA/src/App.js
+++ Virtual-TA/src/App.js
@@ -1,3 +1,4 @@
+import React from "react"
 import logo from './robotta.PNG';
 import './App.css';
 import Footer from './footer'
@@ -6,11 +7,31 @@ import './main.css';
 import config from "./config";
 import MessageParser from "./MessageParser";
 import ActionProvider from "./ActionProvider";
-
+import { MathJax, MathJaxContext } from "better-react-mathjax";
+const equation = "`k_{n+1} = n^2 + k_n^2 - k_{n-1}`";
+const blockFormula = `\\int_0^\\infty x^2 dx`; 
+const conf = {
+  loader: { load: ["input/asciimath"] },
+  asciimath: {
+    displaystyle: true,
+    delimiters: [
+      ["$", "$"],
+      ["`", "`"]
+    ]
+  }
+};
 function App() {
   return (
 
     <div className="App">
+
+            <div className='equations'>
+    
+              <div>
+      </div>
+        </div>
+        #To do for Wednesday: figure out how to feed input into mathjax equation
+        <MathJaxContext config={conf}><MathJax inline>{equation}</MathJax></MathJaxContext>
         <div className = "img"><img src={logo} className="App-logo" alt="logo" /></div>
         <div className = "Bot">
         <Chatbot
@@ -18,6 +39,7 @@ function App() {
         messageParser={MessageParser}
         actionProvider={ActionProvider}
       />
+
       </div>
       <Footer/>
     </div>
