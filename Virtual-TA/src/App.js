86048ba814e7363ff005136767adce2dad668baa
Elizabeth Clements <elizabeth.clementsclan@gmail.com>
2022-10-20 20:50:11 -0700

    Update App.js

--- Virtual-TA/src/App.js
+++ Virtual-TA/src/App.js
@@ -7,7 +7,6 @@ import './main.css';
 import config from "./config";
 import MessageParser from "./MessageParser";
 import ActionProvider from "./ActionProvider";
-import { MathJax, MathJaxContext } from "better-react-mathjax";
 import Form from "./Form.js"
 const equation = "`k_{n+1} = n^2 + k_n^2 - k_{n-1}`";
 const blockFormula = `\\int_0^\\infty x^2 dx`; 
@@ -28,7 +27,6 @@ function App() {
       <div className="form">
               <Form></Form>
               </div>
-              help
         <div className = "img"><img src={logo} className="App-logo" alt="logo" /></div>
         <div className = "Bot">
         <Chatbot
