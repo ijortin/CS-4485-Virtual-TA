c7504c8382db2eb1f63ff60cee0836e34c6d0e9e
timchoi14 <32580201+timchoi14@users.noreply.github.com>
2022-11-02 22:25:33 -0500

    troubleshooting

---
+++ Virtual-TA/backend/chatbot.py
@@ -0,0 +1,157 @@
+from typing import TextIO
+
+import nltk
+
+#nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+from tensorflow.python.framework import ops
+import random
+import json
+import pickle
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug ==True)
+
+with open(r'C:\Users\Timothy Choi\chatting\chat\.venv\intents.json') as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle", "rt") as f:
+        words, labels, training, output = pickle.load(f)
+
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    ops.reset_default_graph()
+
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 5)
+    net = tflearn.fully_connected(net, 6)
+        # net = tflearn.fully_connected(net, 6)
+    net = tflearn.fully_connected(net, 5)
+    net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+    net = tflearn.regression(net)
+
+    model = tflearn.DNN(net)
+    model.fit(training, output, n_epoch=100, batch_size=1, show_metric=True)
+    model.save("model.tflearn")
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+inp = ""
+
+#@app.route('/predict', methods = ['POST'])
+#def getinp(i):
+#    inp = i
+    #inp = input("You: ")
+
+#@app.route('/data')
+#def getout():
+#    #outty = o
+#    results = model.predict([bag_of_words(inp, words)])
+#    results_index = numpy.argmax(results)
+##    tag = labels[results_index]
+#
+#    for tg in data["intents"]:
+#        if tg['tag'] == tag:
+#            responses = tg['responses']
+#            if responses == "works":
+#                # inp O() o() theta()
+#                # i_words = inp.split(" ")
+#                # print(compared(i_words))
+#                print("works")
+#            else:
+#                outs = random.choice(responses)
+#                #getout(outs)
+#                #print(outs)
+
+@app.post("/data")
+def chat():
+    #print("Start talking with the bot (type quit to stop)!")
+    #while True:
+    #inp = input("You: ")
+    #getinp(inp)
+    #if inp.lower() == "quit":
+    #    break
+    inp = request.get_json().get("title")
+
+    results = model.predict([bag_of_words(inp, words)])
+    results_index = numpy.argmax(results)
+    tag = labels[results_index]
+
+    for tg in data["intents"]:
+        if tg['tag'] == tag:
+            responses = tg['responses']
+            outs = random.choice(responses)
+            re = {'Message': outs}
+            return re
+            #getout(outs)
+            #return { "Message": outs,}
+            #print(outs)
+
+#chat()
