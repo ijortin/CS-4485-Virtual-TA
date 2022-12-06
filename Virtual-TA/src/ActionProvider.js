c89d428272924e0c821d2c5721e94b9c42704c99
Elizabeth Clements <63883331+purplesport@users.noreply.github.com>
2022-12-05 17:00:59 -0800

    commented code

--- Virtual-TA/src/ActionProvider.js
+++ Virtual-TA/src/ActionProvider.js
@@ -5,28 +5,30 @@ import React, { useState} from "react";
     const [datas, setdatas] = useState({
       message:"",
   });
-
+//API call to send msg to backend and receive response to compose new message
   const handleDefault = async (msg) => {
+   //Headers and options for request
       const requestOptions = {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ title: msg })
       };
+   //wait for data sent from backend
       const response = await (await fetch('/data', requestOptions)).json()
            setdatas({
             message: response.Message,})
             console.log(response)
 
-
+//render botMessage using botResponse
     const botMessage = createChatBotMessage(response.Message);
-
+//maintain history for render all past messages
     setState((prev) => ({
       ...prev,
       messages: [...prev.messages, botMessage],
     }));
   };
   
-  // Put the handleHello function in the actions object to pass to the MessageParser
+  // Pass the handle default message function to messageparser
   return (
     <div>
       {React.Children.map(children, (child) => {
