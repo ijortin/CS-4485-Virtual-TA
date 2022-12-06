e34f976fa0a54ee1c4a5b08832b714cc5f056769
Elizabeth Clements <63883331+purplesport@users.noreply.github.com>
2022-12-05 17:02:50 -0800

    commented

--- Virtual-TA/src/MessageParser.js
+++ Virtual-TA/src/MessageParser.js
@@ -1,12 +1,14 @@
-// MessageParser starter code
+
 import React from "react";
 
   const MessageParser = ({ children, actions }) => {
-
+   //On any message given to chatbot pass it to handle default in action provider to getresponse from backend 
     const parse = (message) => {
+      //you could add if or case statements here to let the frontend handle more of the bot responses - need to create accompanying funcs in action provider
         actions.handleDefault(message);
     };
   
+    
     return (
       <div>
         {React.Children.map(children, (child) => {
